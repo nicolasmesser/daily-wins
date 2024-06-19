8f006c798dd5ac04e85c6570417f0e1cce719a39
Nicolas Messer <nme@nicolasmesser.ch>
2024-06-19 22:32:23 +0200

    Installing SvelteKit

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 This is a prototype for a daily wins application.
 
 ## Concept
+
 You need 3 daily wins:
+
 - a physical win (walking, running, swimming, ...)
 - a mental win (reading, writing, creating, learning, ...)
 - a spiritual win (meditating, studying, growing, ...)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Daily Wins</h1>
+<p>You need 3 daily wins:</p>
+<ul>
+  <li>a physical win (walking, running, swimming, ...)</li>
+  <li>a mental win (reading, writing, creating, learning, ...)</li>
+  <li>a spiritual win (meditating, studying, growing, ...)</li>
+</ul>
+<p>Be a complete winner!</p>
