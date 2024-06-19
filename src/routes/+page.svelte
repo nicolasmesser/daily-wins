24b2f54b316dc49d65f198ec8d17f9834d439519
Nicolas Messer <nme@nicolasmesser.ch>
2024-06-19 22:44:32 +0200

    Installing TailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<h1>Daily Wins</h1>
-<p>You need 3 daily wins:</p>
-<ul>
-  <li>a physical win (walking, running, swimming, ...)</li>
-  <li>a mental win (reading, writing, creating, learning, ...)</li>
-  <li>a spiritual win (meditating, studying, growing, ...)</li>
-</ul>
-<p>Be a complete winner!</p>
+<div class="container mx-auto">
+  <h1 class="text-3xl font-bold">Daily Wins</h1>
+  <p>You need 3 daily wins:</p>
+  <ul class="list-disc list-inside">
+    <li>a physical win (walking, running, swimming, ...)</li>
+    <li>a mental win (reading, writing, creating, learning, ...)</li>
+    <li>a spiritual win (meditating, studying, growing, ...)</li>
+  </ul>
+  <p>Be a complete winner!</p>
+</div>
