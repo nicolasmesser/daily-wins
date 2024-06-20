3e2c49a3ca415053992ea2503824481340a1e27e
Nicolas Messer <nme@nicolasmesser.ch>
2024-06-20 16:55:47 +0200

    Updating Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
+const colors = require('tailwindcss/colors');
+const svelteUx = require('svelte-ux/plugins/tailwind.cjs');
+
+module.exports = {
+  content: [
+    './src/**/*.{html,svelte}',
+    './node_modules/svelte-ux/**/*.{svelte,js}'
+  ],
+
+  // See customization docs: https://svelte-ux.techniq.dev/customization
+  ux: {
+    themes: {
+      light: {
+        primary: colors['orange']['500'],
+        'primary-content': 'white',
+        secondary: colors['blue']['500'],
+        'surface-100': 'white',
+        'surface-200': colors['gray']['100'],
+        'surface-300': colors['gray']['300'],
+        'surface-content': colors['gray']['900'],
+        'color-scheme': 'light'
+      },
+      dark: {
+        primary: colors['orange']['500'],
+        'primary-content': 'white',
+        secondary: colors['blue']['500'],
+        'surface-100': colors['zinc']['800'],
+        'surface-200': colors['zinc']['900'],
+        'surface-300': colors['zinc']['950'],
+        'surface-content': colors['zinc']['100'],
+        'color-scheme': 'dark'
+      },
+    },
   },
-  plugins: [],
-}
+
+  plugins: [
+    svelteUx,  // uses hsl() color space by default. To use oklch(), use: svelteUx({ colorSpace: 'oklch' }),
+  ]
+};
